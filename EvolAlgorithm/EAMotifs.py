c7d2b0b086c80b9b495d87a439b84f32ccce64b5
andrefiliperamos <andrefiliperamos@users.noreply.github.com>
2024-03-23 16:00:12 +0000

    Add files via upload

---
+++ EvolAlgorithm/EAMotifs.py
@@ -0,0 +1,93 @@
+from EvolAlgorithm import EvolAlgorithm
+from Popul import PopulInt, PopulReal
+from MotifFinding import MotifFinding
+from MyMotifs import MyMotifs
+
+
+def createMatZeros(nl, nc):
+    res = []
+    for _ in range(0, nl):
+        res.append([0]*nc)
+    return res
+
+
+def printMat(mat):
+    for i in range(0, len(mat)):
+        for j in range(len(mat[i])):
+            print(f"{mat[i][j]:.3f}", end=' ')
+        print()
+
+
+class EAMotifsInt (EvolAlgorithm):
+    def __init__(self, popsize, numits, noffspring, filename):
+        self.motifs = MotifFinding()
+        self.motifs.readFile(filename, "dna")
+        indsize = len(self.motifs)
+        EvolAlgorithm.__init__(self, popsize, numits, noffspring, indsize)
+
+    def initPopul(self, indsize):
+        maxvalue = self.motifs.seqSize(0) - self.motifs.motifSize
+        self.popul = PopulInt(self.popsize, indsize,
+                              maxvalue, [])
+
+    def evaluate(self, indivs):
+        for i in range(len(indivs)):
+            ind = indivs[i]
+            sol = ind.getGenes()
+            fit = self.motifs.score(sol)
+            ind.setFitness(fit)
+
+
+
+'''
+Em seguida deverá criar uma classe que estende a classe 
+EvolAlgorithm e que redefine o construtor, o método initPopul e 
+o método evaluate 
+• Note que, neste caso, o tamanho do indivíduo deverá ser: L x A, 
+onde L é o tamanho do motif e A é o nº de símbolos do alfabeto
+
+'''
+class EAMotifsReal(EvolAlgorithm):
+    def _init_(self, popsize, numits, noffspring, filename):
+
+        self.motifs_finding = MotifFinding()
+        self.motifs.readFile(filename, "dna")
+        indsize = self.motifs.motifSize * len(self.motifs.alphabet)
+        EvolAlgorithm._init_(self, popsize, numits, noffspring, indsize)
+
+    def initPopul(self, motSize, nSymb): 
+        self.popul = PopulReal(self.popsize, motSize*nSymb)
+
+
+    def evaluate(self, indivs):
+       for i in range(len(indivs)):
+            ind = indivs[i]
+            sol = ind.getGenes()
+            # criar motifs
+            pwm = self.vec_to_pwm(sol)
+            mtf = MyMotifs(pwm=pwm, alphabet=self.motifs_finding.alphabet)
+            s = []
+            for j in range(len(self.motifs_finding.seqs)):
+                seq = self.motifs_finding.seqs[j].seq
+                p = self.motifs.mostProbableSeq(seq)
+                s.append(p)
+            fit = self.motifs_finding.score(sol)
+            ind.setFitness(fit)
+    
+
+    
+
+def test1():
+    ea = EAMotifsInt(100, 1000, 50, "EvolAlgorithm/exemploMotifs.txt")
+    ea.run()
+    ea.printBestSolution()
+
+
+def test2():
+    ea = EAMotifsReal(100, 2000, 50, "EvolAlgorithm/exemploMotifs.txt", 2)
+    ea.run()
+    ea.printBestSolution()
+
+
+test1()
+# test2()
