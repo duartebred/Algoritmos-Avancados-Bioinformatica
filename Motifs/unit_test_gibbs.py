38c95096556d7e5fc7b60cfed4c012dc0adf6a3a
Ricardo Oliveira <ricardofoliveira61@gmail.com>
2024-04-03 11:28:00 +0100

    [ADD] unit_test_gibbs.py
    Initial commit of unit test for Gibbs sampling

---
+++ Motifs/unit_test_gibbs.py
@@ -0,0 +1,12 @@
+import unittest
+from Gibbs_sampling import pwm
+from Gibbs_sampling import Gibbs
+
+
+class Test_gibss_sampling(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
