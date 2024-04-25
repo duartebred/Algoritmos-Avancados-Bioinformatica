f66afb82b2785378d165694d2fe8a0ed7a986a22
Ricardo <ricardofoliveira61@gmail.com>
2024-04-25 22:28:04 +0100

    [UPDATE] unit_test_gibbs.py
    New unit test for pwm class

--- Motifs/unit_test_gibbs.py
+++ Motifs/unit_test_gibbs.py
@@ -30,22 +30,22 @@ def round_pwm_results(pwm:list[dict[str,float]]):
     return pwm
 
 
-def pwm_DNA(seq_type:int, pseudo:float):
-    assert seq_type == "DNA",f"A sequência biológica deve ser DNA"
+def pwm_dna(seq_type:str, pseudo:float):
     assert isinstance(seq_type,str)
+    assert seq_type.upper() == "DNA",f"A sequência biológica deve ser DNA"
     assert isinstance(pseudo,int) or isinstance(pseudo,float)
     assert pseudo in [0,1,2]
     
     if pseudo == 0:
         pwm_0=[
                [],
-               [{"A": 1.0000 , "C":0.0000 , "G":0.0000 , "T":0.0000},
-                {"A": 1.0000 , "C":0.0000 , "G":0.0000 , "T":0.0000},
-                {"A": 1.0000 , "C":0.0000 , "G":0.0000 , "T":0.0000},
-                {"A": 1.0000 , "C":0.0000 , "G":0.0000 , "T":0.0000}],
-               [{"A":, "C": , "G": , "T": },
-                {"A": 0.1429, "C": 0.2857, "G": 0.2857, "T": 0.2857},
-                {"A": 0.1429, "C": 0.2857, "G": 0.1429, "T": 0.4286}],
+               [{"A": 1.0000, "C": 0.0000, "G": 0.0000, "T": 0.0000},
+                {"A": 1.0000, "C": 0.0000, "G": 0.0000, "T": 0.0000},
+                {"A": 1.0000, "C": 0.0000, "G": 0.0000, "T": 0.0000},
+                {"A": 1.0000, "C": 0.0000, "G": 0.0000, "T": 0.0000}],
+               [{"A": 0.3333, "C": 0.3333, "G": 0.0000, "T": 0.3333 },
+                {"A": 0.0000, "C": 0.3333, "G": 0.3333, "T": 0.3333},
+                {"A": 0.0000, "C": 0.3333, "G": 0.0000, "T": 0.6667}],
                ]
         
         return pwm_0
@@ -69,16 +69,70 @@ def pwm_DNA(seq_type:int, pseudo:float):
                   {"A": 0.4545, "C": 0.1818, "G": 0.1818, "T": 0.1818},
                   {"A": 0.4545, "C": 0.1818, "G": 0.1818, "T": 0.1818},
                   {"A": 0.4545, "C": 0.1818, "G": 0.1818, "T": 0.1818}],
-                 []]
+                 [{"A": 0.2727, "C": 0.2727, "G": 0.1818, "T": 0.2727},
+                  {"A": 0.1818, "C": 0.2727, "G": 0.2727, "T": 0.2727},
+                  {"A": 0.1818, "C": 0.2727, "G": 0.1818, "T": 0.3636}]]
         
         return pwm_2
 
+
+def pwm_proteina(seq_type:str, pseudo: float):
+    assert isinstance(seq_type,str)
+    assert seq_type.upper() == "PROTEINA",f"A sequência biológica deve ser do tipo proteína"
+    assert isinstance(pseudo,int) or isinstance(pseudo,float)
+    assert pseudo in [0,1,2]
+    
+    if pseudo == 0:
+        pwm_0=[
+            [],
+            [{"A": 0.2500, "B": 0.0000, "C": 0.0000, "D": 0.2500, "E": 0.0000, "F": 0.0000, "G": 0.2500, "H": 0.0000, "I": 0.0000,
+              "K": 0.0000, "L": 0.2500, "M": 0.0000, "N": 0.0000, "P": 0.0000, "Q": 0.0000, "R": 0.0000, "S": 0.0000, "T": 0.0000,
+              "V": 0.0000, "W": 0.0000, "Y": 0.0000, "Z": 0.0000},
+             {"A": 0.0000, "B": 0.0000, "C": 0.0000, "D": 0.0000, "E": 0.2500, "F": 0.0000, "G": 0.0000, "H": 0.2500, "I": 0.0000,
+              "K": 0.0000, "L": 0.0000, "M": 0.2500, "N": 0.0000, "P": 0.0000, "Q": 0.0000, "R": 0.2500, "S": 0.0000, "T": 0.0000, 
+              "V": 0.0000, "W": 0.0000, "Y": 0.0000, "Z": 0.0000},
+             {"A": 0.0000, "B": 0.0000, "C": 0.0000, "D": 0.0000, "E": 0.0000, "F": 0.2500, "G": 0.0000, "H": 0.0000, "I": 0.2500,
+              "K": 0.0000, "L": 0.0000, "M": 0.0000, "N": 0.5000, "P": 0.0000, "Q": 0.0000, "R": 0.0000, "S": 0.0000, "T": 0.0000,
+              "V": 0.0000, "W": 0.0000, "Y": 0.0000, "Z": 0.0000}]]
+        
+        return pwm_0
+
+    if pseudo == 1:
+        pwm_1 = [
+            [],
+            [{"A": 0.0769, "B": 0.0385, "C": 0.0385, "D": 0.0769, "E": 0.0385, "F": 0.0385, "G": 0.0769, "H": 0.0385, "I": 0.0385,
+              "K": 0.0385, "L": 0.0769, "M": 0.0385, "N": 0.0385, "P": 0.0385, "Q": 0.0385, "R": 0.0385, "S": 0.0385, "T": 0.0385,
+              "V": 0.0385, "W": 0.0385, "Y": 0.0385, "Z": 0.0385},
+              {"A": 0.0385, "B": 0.0385, "C": 0.0385, "D": 0.0385, "E": 0.0769, "F": 0.0385, "G": 0.0385, "H": 0.0769, "I": 0.0385,
+               "K": 0.0385, "L": 0.0385, "M": 0.0769, "N": 0.0385, "P": 0.0385, "Q": 0.0385, "R": 0.0769, "S": 0.0385, "T": 0.0385, 
+               "V": 0.0385, "W": 0.0385, "Y": 0.0385, "Z": 0.0385},
+             {"A": 0.0385, "B": 0.0385, "C": 0.0385, "D": 0.0385, "E": 0.0385, "F": 0.0769, "G": 0.0385, "H": 0.0385, "I": 0.0769,
+              "K": 0.0385, "L": 0.0385, "M": 0.0385, "N": 0.1154, "P": 0.0385, "Q": 0.0385, "R": 0.0385, "S": 0.0385, "T": 0.0385,
+              "V": 0.0385, "W": 0.0385, "Y": 0.0385, "Z": 0.0385}]]
+        
+        return pwm_1
+    
+    if pseudo == 2:
+        pwm_2 = [
+            [],
+            [{"A": 0.0625, "B": 0.0417, "C": 0.0417, "D": 0.0625, "E": 0.0417, "F": 0.0417, "G": 0.0625, "H": 0.0417, "I": 0.0417,
+              "K": 0.0417, "L": 0.0625, "M": 0.0417, "N": 0.0417, "P": 0.0417, "Q": 0.0417, "R": 0.0417, "S": 0.0417, "T": 0.0417,
+              "V": 0.0417, "W": 0.0417, "Y": 0.0417, "Z": 0.0417},
+              {"A": 0.0417, "B": 0.0417, "C": 0.0417, "D": 0.0417, "E": 0.0625, "F": 0.0417, "G": 0.0417, "H": 0.0625, "I": 0.0417,
+               "K": 0.0417, "L": 0.0417, "M": 0.0625, "N": 0.0417, "P": 0.0417, "Q": 0.0417, "R": 0.0625, "S": 0.0417, "T": 0.0417, 
+               "V": 0.0417, "W": 0.0417, "Y": 0.0417, "Z": 0.0417},
+             {"A": 0.0417, "B": 0.0417, "C": 0.0417, "D": 0.0417, "E": 0.0417, "F": 0.0625, "G": 0.0417, "H": 0.0417, "I": 0.0625,
+              "K": 0.0417, "L": 0.0417, "M": 0.0417, "N": 0.0833, "P": 0.0417, "Q": 0.0417, "R": 0.0417, "S": 0.0417, "T": 0.0417,
+              "V": 0.0417, "W": 0.0417, "Y": 0.0417, "Z": 0.0417}]]
+        
+        return pwm_2
 
 class Test_pwm_gibss_sampling(unittest.TestCase):
     def setUp(self):
         self.test = ['GTAAACAATATTTATAGC', 'AAAATTTACCTTAGAAGG', 'CCGTACTGTCAAGCGTGG', 'TGAGTAAACGACGTCCCA', 'TACTTAACACCCTGTCAA']
         self.seqs_DNA = [[], ["AAAA","AAAA","AAAA"], ["ACT","TTT","CGC"]]
-        
+        self.seqs_proteina = [[], ["ARN", "DEF", "GHI", "LMN"]]
+       
     def test_pwm_alfabeto(self):
         seq_type = ['DNA', 'RNA', 'PROTEIN', "dna", "RnA","ProtEiN"]
         alfa = ["ACGT", "ACGU","ABCDEFGHIKLMNPQRSTVWYZ","ACGT", "ACGU","ABCDEFGHIKLMNPQRSTVWYZ" ]
@@ -94,10 +148,20 @@ class Test_pwm_gibss_sampling(unittest.TestCase):
 
     def test_pwm_genarete_pwm(self):
         pseudo = [0,1,2]
+        # passar isto para uma função visto que são dois pedaços de código iguais
         for idx,seq_DNA in enumerate(self.seqs_DNA):
             for pcount in pseudo:   
-                self.assertEqual(round_pwm_results(pwm(seq_DNA,pseudo=pcount).generate_pwm()), pwm_DNA("DNA", pseudo= pcount)[idx],
-                                 msg=f"Erro para a sequeência {seq_DNA} para a pseudocontagem de {pcount}")
+                self.assertEqual(round_pwm_results(pwm(seq_DNA,pseudo=pcount).generate_pwm()), pwm_dna("DNA", pseudo= pcount)[idx],
+                    msg=f"Erro para a sequência {seq_DNA} na pseudocontagem de {pcount}")
+
+        for idx,seq_proteina in enumerate(self.seqs_proteina):
+            for pcount in pseudo:
+                expected_pwm =  pwm_proteina("proteina", pseudo= pcount)[idx]
+                generated_pwm = round_pwm_results(pwm(seq_proteina, seq_type="protein", pseudo=pcount).generate_pwm())
+                self.assertEqual(generated_pwm, expected_pwm, msg=f"Erro para a sequência {seq_proteina} para na pseudocontagem de {pcount}")
+    
+    def test_pwm_prob_seq(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
