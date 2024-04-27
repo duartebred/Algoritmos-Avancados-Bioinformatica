2c5bc5000d7a9554b8e64a482ac47651d32bc39e
Duarte <duartealvesvelho@gmail.com>
2024-04-27 15:30:12 +0100

    [ADD] Trees.py

---
+++ Procura Padrões/Trees.py
@@ -0,0 +1,128 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import graphviz
+from pprint import pprint
+class Trees:
+
+    def __init__(self, listaPalavras):
+
+        self.listaPalavras = listaPalavras
+        self.trie = {}
+
+
+    def inserir(self):
+
+        for palavra in self.listaPalavras:
+            t = self.trie
+            for letra in palavra:
+
+                if letra not in t:
+
+                    t[letra] = {}
+                t = t[letra]
+
+            t['$'] = 0
+
+        return self.trie, t
+    
+    def procurar(self, palavra):
+
+        t = self.trie
+        
+        for letra in palavra:
+            
+            if letra not in t:
+                
+                return False
+            
+            t = t[letra]
+        # No caso de se querer a string completa utiliza-se:
+        if '$' in t:
+            
+             
+            return True
+            
+        else:
+            
+            return False
+    
+    def apagar(self, palavra):
+        
+        def _apagar(nodo, palavra, i):
+            
+            if i == len(palavra):
+                
+                if '$' in nodo:
+                    
+                    del nodo['$']
+                    
+                return '$' not in nodo
+            
+            if palavra[i] in nodo:
+                
+                if _apagar(nodo[palavra[i]], palavra, i + 1):
+                    
+                    del nodo[palavra[i]]
+                    
+                return len(nodo.keys()) == 0
+            
+            return False
+
+        _apagar(self.trie, palavra, 0)
+        
+    
+    def vistaGrafViz(self, nodo=None, parent=None, edge_label=''):
+
+        if nodo is None:
+            nodo = self.trie
+
+        dot = graphviz.Digraph('Trie', comment='Visualização da Trie')
+
+        for chave, valor in nodo.items():
+            
+            if chave == '$':
+                                
+                continue
+            
+            dot.node(chave, chave)
+            
+            if parent is not None:
+                
+                dot.edge(parent, chave, label=edge_label)
+                
+            self.vistaGrafViz(valor, chave, edge_label=chave)
+
+        #print(dot.source)
+
+        return dot
+    
+    def vistaGrafica(self, nodo=None, parent=None, edge_label='', G=None):
+
+        if nodo is None:
+            nodo = self.trie
+            G = nx.DiGraph()
+
+        for chave, valor in nodo.items():
+            
+            if chave == '$':
+                continue
+            
+            G.add_node(chave)
+            
+            if parent is not None:
+                G.add_edge(parent, chave)
+                
+            self.vistaGrafica(valor, chave, edge_label=chave, G=G)
+
+        return G
+   
+t = Trees(['ar', 'amora', 'amor', 'braquio'])
+t.inserir()
+print(t.procurar('amora'))
+pprint(t.trie)
+G = t.vistaGrafViz()
+G
+
+G = t.vistaGrafica()
+nx.draw(G, with_labels=True)
+plt.show()
