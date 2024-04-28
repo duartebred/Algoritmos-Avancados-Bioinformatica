80d5d9c8b4b2461d17ba49ce4e896470795a0a3e
Ricardo <ricardofoliveira61@gmail.com>
2024-04-28 23:29:17 +0100

    [UPDATE]unit_test_gibbs.py
    unit test for class pwm finished

--- Motifs/unit_test_gibbs.py
+++ Motifs/unit_test_gibbs.py
@@ -161,16 +161,55 @@ class Test_pwm_gibss_sampling(unittest.TestCase):
                 generated_pwm = round_pwm_results(pwm(seq_proteina, seq_type="protein", pseudo=pcount).generate_pwm())
                 self.assertEqual(generated_pwm, expected_pwm, msg=f"Erro para a sequência {seq_proteina} para na pseudocontagem de {pcount}")
     
+    def test_pwm_genarete_pwm_error(self):
+        
+        for seqs in self.seqs_DNA[1:2]:
+            seqs.append("AA")
+            with self.assertRaises(AssertionError):
+                pwm(seqs).generate_pwm()
+
+        with self.assertRaises(AssertionError):
+            pwm(self.seqs_proteina+["AA"],seq_type="protein").generate_pwm()
+
+        with self.assertRaises(AssertionError):
+            pwm("AAAA ACTG", seq_type="DNA").generate_pwm()
+
     def test_pwm_prob_seq(self):
-        pass
+               
+        prob_expected = [[0.00016056972144289452, 3.0106822770542722e-05, 6.0213645541085445e-05, 2.0071215180361815e-05, 4.014243036072363e-05,
+                          3.0106822770542716e-05, 4.014243036072362e-05, 3.5682160320643224e-05, 9.032046831162817e-05, 1.3380810120241209e-05,
+                          1.0035607590180907e-05],
+                          [0.0001605697214428945, 0.00010704648096192964, 1.0035607590180907e-05, 2.508901897545227e-06, 3.7633528463178395e-06,
+                           1.3380810120241209e-05, 2.6761620240482418e-05, 2.6761620240482418e-05, 1.3380810120241209e-05, 6.6904050601206044e-06,
+                           2.0071215180361815e-05],
+                           [3.0106822770542722e-05, 5.575337550100504e-07, 8.920540080160806e-06, 1.0035607590180907e-05, 3.5682160320643224e-05,
+                            1.3380810120241209e-05, 2.508901897545227e-06, 3.3452025300603022e-06, 5.017803795090454e-06, 6.6904050601206044e-06,
+                            1.1150675100201007e-06],
+                            [0.00021409296192385934, 6.021364554108543e-05, 5.017803795090454e-06, 4.014243036072363e-05, 2.0071215180361815e-05,
+                             3.3452025300603022e-06, 5.017803795090454e-06, 3.3452025300603022e-06, 1.4867566800268008e-06, 1.3380810120241209e-05,
+                             6.6904050601206044e-06],
+                             [0.00012042729108217089, 1.0035607590180907e-05, 2.0071215180361815e-05, 1.0035607590180907e-05, 1.5053411385271361e-05,
+                              2.508901897545227e-06, 6.6904050601206044e-06, 5.575337550100504e-07, 4.460270040080403e-06, 4.460270040080403e-06,
+                              3.5682160320643224e-05]]
+
+        for idx,seq in enumerate(self.test):
+            for start_p in range(0,len(seq)-7):
+                self.assertEqual(round(pwm(self.test).prob_seq(seq[start_p:start_p+8]),5),round(prob_expected[idx][start_p],5),
+                    msg=f"""
+                        A sequência {seq} possui probabilidade {pwm(self.test).prob_seq(seq[start_p:start_p+8])} enquanto a probablidade
+                        esperada é de {prob_expected[idx][start_p]} """)
+    
+    def test_pwm_prob_seq_error(self):
+        for seq in self.test:
+            with self.assertRaises(AssertionError):
+                pwm(self.test).prob_seq(seq+"AGTC")
 
 
 class Test_gibbs(unittest.TestCase):
 
-    def test_new_seqs():
+    def test_new_seqs(self):
         pass
 
 
 if __name__ == '__main__':
     unittest.main()
-
